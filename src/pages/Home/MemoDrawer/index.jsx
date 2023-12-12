1ac0ef3a3cf738cb593ced3b238dfcb3073ed1e8
ChenGuangLong <chenguanglong@timeexpress.com.cn>
2023-12-12 19:01:05 +0800

    备忘 加列表

--- src/pages/Home/MemoDrawer/index.jsx
+++ src/pages/Home/MemoDrawer/index.jsx
@@ -1,38 +1,95 @@
 import {observer} from 'mobx-react-lite'
+
 import showOrNot from "../../../store/ShowOrNot";
-import {Drawer} from "antd";
-import React from "react";
+import {Drawer, Avatar, Divider, List, Skeleton, Button, Space} from "antd";
+import React, {useEffect, useState} from "react";
+import './MemoDrawer.css'
+import axios from "axios";
+
+const count = 5;
+const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
 
 const MemoDrawer = observer(() => {
-
+        const [initLoading, setInitLoading] = useState(true);
+        const [loading, setLoading] = useState(false);
+        const [data, setData] = useState([]);
+        const [list, setList] = useState([]);
+        useEffect(() => {
+            fetch(fakeDataUrl)
+                .then((res) => res.json())
+                .then((res) => {
+                    setInitLoading(false);
+                    setData(res.results);
+                    setList(res.results);
+                });
+        }, []);
+        const onLoadMore = () => {
+            setLoading(true);
+            setList(
+                data.concat(
+                    [...new Array(count)].map(() => ({
+                        loading: true,
+                        name: {},
+                        picture: {},
+                    })),
+                ),
+            );
+            fetch(fakeDataUrl)
+                .then((res) => res.json())
+                .then((res) => {
+                    const newData = data.concat(res.results);
+                    setData(newData);
+                    setList(newData);
+                    setLoading(false);
+                    // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
+                    // In real scene, you can using public method of react-virtualized:
+                    // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
+                    window.dispatchEvent(new Event('resize'));
+                });
+        };
+        const loadMore =
+            !initLoading && !loading ? (
+                <div
+                    style={{
+                        textAlign: 'center',
+                        marginTop: 12,
+                        height: 32,
+                        lineHeight: '32px',
+                    }}
+                >
+                    <Button onClick={onLoadMore}>loading more</Button>
+                </div>
+            ) : null;
 
         return (
             <Drawer title="备忘录" placement="right" onClose={() => showOrNot.setMemoDrawerShow(false)}
-                    open={showOrNot.memoDrawerShow} style={{opacity: 0.8}}>
-                <p>按时睡觉，不按时睡觉，你就是个loser</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+                    open={showOrNot.memoDrawerShow} style={{opacity: 0.8}}
+            >
+
+                <List
+                    className="demo-loadmore-list"
+                    loading={initLoading}
+                    itemLayout="horizontal"
+                    loadMore={loadMore}
+                    dataSource={list}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Skeleton avatar title={false} loading={item.loading} active>
+                                <List.Item.Meta
+                                    description={
+                                        <div>
+                                            {item.name?.last}内容，内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容
+                                            <br/>
+                                            [<a key="list-loadmore-success">完成</a>|
+                                            <a key="list-loadmore-edit">编辑</a>|
+                                            <a key="list-loadmore-more">删除</a>]
+                                        </div>
+                                    }
+                                />
+                            </Skeleton>
+                        </List.Item>
+                    )}
+                />
 
             </Drawer>
         )
