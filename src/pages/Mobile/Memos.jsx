6b0c481c736b010daf4104c145f4a92290664a7f
仰晨 <cgl556@88.com>
2024-01-16 01:32:37 +0800

    手机列表 列表切换后顺便搞个状态刷新 不然页面动不了

--- src/pages/Mobile/Memos.jsx
+++ src/pages/Mobile/Memos.jsx
@@ -18,9 +18,10 @@ export default () =>{
     const [incompleteCounts,setIncompleteCounts] = useState({0:null,1:null,2:null,3:null,5:null,6:null,7:null})
     // 新增或修改的数据 不再目前的待办就要处理的值，把那个类型的待办重置
     const [changeType, setChangeType] = useState(-1)
+    const [ refresh, setRefresh] = useState(true)
 
     return (
-        <Tabs defaultActiveKey='普通' className={'█tabsContainer'}>
+        <Tabs defaultActiveKey='普通' className={'█tabsContainer'} onChange={(key) => setRefresh(!refresh)}>
 
             {tabList.map(tab=>
                 <Tabs.Tab title={<Badge content={incompleteCounts[tab.type]}>{tab.title}</Badge>} key={tab.title}>
