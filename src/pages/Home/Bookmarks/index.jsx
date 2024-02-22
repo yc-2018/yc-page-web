4a4c4fcd3210945257123b9657475db9cb1baa28
仰晨 <cgl556@88.com>
2024-02-23 03:01:40 +0800

    书签横向拖动   暂时不知道为什么没有动画效果

--- src/pages/Home/Bookmarks/index.jsx
+++ src/pages/Home/Bookmarks/index.jsx
@@ -1,10 +1,24 @@
+import React, {useState} from "react";
 import { Button, Dropdown, Space } from 'antd';
+import { DndContext, closestCenter, PointerSensor, useSensor, useSensors } from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    horizontalListSortingStrategy
+} from '@dnd-kit/sortable';
+
 import CommonStore from "../../../store/CommonStore";
 import Dnd from "../../../compontets/Dnd";
+import Menu from "./Menu";
 
 
 export default function Bookmarks() {
-    const items = [
+    const [items, setItems] = useState([
+        { id: "1hhh", name: 'Item 1' },
+        { id: "2hhh", name: 'Item 2' },
+        { id: "3hhh", name: 'Item 3' },
+    ]);
+    const itemss = [
         {
             key: '1',
             label: (
@@ -37,15 +51,40 @@ export default function Bookmarks() {
             label: 'a danger item',
             icon: <img alt src="https://x.chatmindai.net/Logo.png" width={10} />,
         },
-
-
-
     ];
+
+    // 使用自定义Hook来获取传感器数据
+    const sensors = useSensors(
+        // 使用自定义Hook来获取鼠标传感器数据
+        useSensor(PointerSensor),
+        // 使用自定义Hook来获取键盘传感器数据，并指定坐标获取器（暂时没用）
+        // useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+    );
+
+
+
+    /**
+     * 当拖拽结束时触发的回调函数
+     * */
+    const handleDragEnd = event => {
+        const { active, over } = event;
+
+        if (active.id !== over.id) {
+            setItems((items) => {
+                // 找到拖拽项目（active.id）的索引
+                const oldIndex = items.findIndex(item => item.id === active.id);
+                // 找到目标位置项目（over.id）的索引
+                const newIndex = items.findIndex(item => item.id === over.id);
+                console.log('███████newIndex>>>>', arrayMove(items, oldIndex, newIndex),'<<<<██████')
+                return arrayMove(items, oldIndex, newIndex);
+            });
+        }
+    };
 
     return (
         <Space>
 
-            <Dropdown menu={{ items, }} >
+            <Dropdown menu={{ itemss, }} >
                 <Button 
                     ghost 
                     type="dashed" 
@@ -82,6 +121,24 @@ export default function Bookmarks() {
             <Dropdown dropdownRender={() => <div />}>
                 <a rel="noopener noreferrer"  href='https://ant-design.gitee.io/components/select-cn'>antd选择器</a>
             </Dropdown>
+
+            {/**
+             * sensors:  传感器是用于处理拖拽输入的设备，例如鼠标、触摸屏或键盘。sensors 属性允许你定义用于拖拽操作的传感器数组。
+             * collisionDetection: 碰撞检测是确定拖拽项目在移动过程中与哪些项目发生交互的算法。
+             * onDragEnd: 当拖拽结束时触发的回调函数。
+             * ——————————
+             * items: 这个属性接受一个项目标识符的数组，定义了哪些项目是可排序的。这些标识符通常是字符串或数字，用于唯一标识每个可拖拽项目。
+             * strategy: 排序策略定义了项目排序的逻辑。verticalListSortingStrategy 是 @dnd-kit/sortable 提供的一个策略，用于垂直列表的排序。这个策略决定了项目如何根据拖拽操作重新排序。
+             * */}
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <SortableContext items={items.map(item => item.id)}
+                                 strategy={horizontalListSortingStrategy}
+                >
+                    {items.map(item =>
+                        <Menu key={item.id} obj={item} />
+                    )}
+                </SortableContext>
+            </DndContext>
 
         </Space>
     )
