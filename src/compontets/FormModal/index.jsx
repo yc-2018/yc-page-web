6f394484d5cbbb2cc9f5b8b9438745a45cd97410
仰晨 <cgl556@88.com>
2024-03-15 22:35:32 +0800

    修改备忘录新增修改弹窗

--- src/compontets/FormModal/index.jsx
+++ src/compontets/FormModal/index.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useRef, useState} from "react";
-import {Modal, Input, Radio, Space, Divider, App, Button, DatePicker} from 'antd';
+import {Modal, Input, Radio, App, Button, DatePicker} from 'antd';
 import {saveOrUpdateToDoItem} from "../../request/homeRequest";
+import styles from '../../common.module.css'
 const { TextArea } = Input;
 /**
  * 新增/编辑备忘录弹窗
@@ -13,8 +14,10 @@ const { TextArea } = Input;
  * */
 const FormModal = ({isOpen,setOpen,data,reList,currentMemoType}) => {
     const [formData, setFormData] = useState(null);    // 用来复制编辑的数据（改变的）
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const [openDate, setOpenDate] = useState(false)
+    const [confirmLoading, setConfirmLoading] = useState(false)
+
+    const [openDate, setOpenDate] = useState(false)                 // 日期选择
+    const [openDateRange, setOpenDateRange] = useState(false)       // 日期范围选择
 
     const textRef = useRef(null)  // 搜索框的ref 让它能自动获得焦点
 
@@ -36,6 +39,12 @@ const FormModal = ({isOpen,setOpen,data,reList,currentMemoType}) => {
             window.setTimeout(() => textRef.current?.focus(), 100)
     },[isOpen])
 
+    /** 关闭弹窗 */
+    const closeModal = () => {
+        setOpenDateRange(false)
+        setOpenDate(false)
+        window.setTimeout(() =>setOpen(false),100 )
+    };
 
     /** 确定按钮（编辑完成给后端发请求） */
     const handleOk = async() => {
@@ -50,7 +59,7 @@ const FormModal = ({isOpen,setOpen,data,reList,currentMemoType}) => {
         body.id = data?.id;
         let result = await saveOrUpdateToDoItem(body,data && "put");
         if(result) {
-            setOpen(false);
+            closeModal();
             reList(Math.random()) // 刷新列表
         }
         setConfirmLoading(false);
@@ -64,43 +73,71 @@ const FormModal = ({isOpen,setOpen,data,reList,currentMemoType}) => {
         textRef.current?.focus() // 将光标移动到最后
     }
 
+    /** 插入日期范围 */
+    const insertDateRange = (_dates, date2Str) => {
+        const content = date2Str.join('~') + '\n' + (formData.content ?? '');
+        setFormData({...formData,content})
+        setOpenDateRange(false)
+        textRef.current?.focus() // 将光标移动到最后
+    }
 
     return (
         <Modal
-            title={data?"编辑备忘":"新增备忘"}
+            width={888}
+            title={data ? "编辑备忘" : "新增备忘"}
             open={isOpen}
-            onOk={handleOk}
-            onCancel={()=>setOpen(false)}
+            // onCancel={closeModal}
             confirmLoading={confirmLoading}
             footer={[
-                <Button key="dateButt" onClick={()=>setOpenDate(v=>!v)}>插入日期</Button>,
-                <DatePicker key="date" open={openDate} onChange={insertDate}  placement={'topRight'} style={{visibility:'hidden',width:0}}/>,
-                <Button key="back" onClick={()=>setOpen(false)}>返回</Button>,
+                <Button
+                    key="dateButt"
+                    onClick={() => setOpenDate(v => !v) || setOpenDateRange(false)}
+                >
+                    插入日期
+                </Button>,
+                <Button
+                    key="RangeButt"
+                    onClick={() => setOpenDateRange(v => !v)||setOpenDate(false) }
+                >
+                    插入日期段
+                </Button>,
+                <DatePicker key="date" open={openDate} onChange={insertDate} placement={'topRight'} style={{visibility: 'hidden', width: 0}}/>,
+                <DatePicker.RangePicker
+                    key="dateRange"
+                    open={openDateRange}
+                    onChange={insertDateRange}
+                    placement={'topRight'}
+                    style={{visibility: 'hidden'}}
+                />,
+                <Button key="back" onClick={closeModal}>返回</Button>,
                 <Button key="submit" type="primary" onClick={handleOk}>提交</Button>,
             ]}
         >
-            <Divider plain>
-                <Space direction={'vertical'} style={{marginBottom:20}}>
-                    <Radio.Group onChange={(e)=>{setFormData({...formData,itemType:e.target.value})}} defaultValue={formData?.itemType} value={formData?.itemType} buttonStyle="solid">
-                        <Radio.Button value={0}>普通</Radio.Button>
-                        <Radio.Button value={1}>循环</Radio.Button>
-                        <Radio.Button value={2}>长期</Radio.Button>
-                        <Radio.Button value={3}>紧急</Radio.Button>
-                        <Radio.Button value={5}>日记</Radio.Button>
-                        <Radio.Button value={6}>工作</Radio.Button>
-                        <Radio.Button value={7}>其他</Radio.Button>
-                    </Radio.Group>
-                    <TextArea rows={16}
-                              maxLength={2000}
-                              showCount
-                              placeholder="请输入备忘内容"
-                              value={formData?.content}
-                              ref={textRef}
-                              onChange={(e)=>setFormData({...formData,content:e.target.value})}
-                    />
-                </Space>
-            </Divider>
+            <div className={styles.lrCenter}>
+                <Radio.Group
+                    onChange={(e) => setFormData({...formData, itemType: e.target.value})}
+                    value={formData?.itemType}
+                    buttonStyle="solid"
+                    style={{margin: 5}}
+                >
+                    <Radio.Button value={0}>普通</Radio.Button>
+                    <Radio.Button value={1}>循环</Radio.Button>
+                    <Radio.Button value={2}>长期</Radio.Button>
+                    <Radio.Button value={3}>紧急</Radio.Button>
+                    <Radio.Button value={5}>日记</Radio.Button>
+                    <Radio.Button value={6}>工作</Radio.Button>
+                    <Radio.Button value={7}>其他</Radio.Button>
+                </Radio.Group>
+            </div>
+            <TextArea rows={16}
+                      showCount
+                      ref={textRef}
+                      maxLength={2000}
+                      value={formData?.content}
+                      placeholder="请输入备忘内容"
+                      onChange={e => setFormData({...formData, content: e.target.value})}
+            />
         </Modal>
-    );
-};
+    )
+}
 export default FormModal;
