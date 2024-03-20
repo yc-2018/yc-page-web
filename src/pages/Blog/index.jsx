166ca150a54465485e06ae7333c3da7621e25922
仰晨 <cgl556@88.com>
2024-03-20 14:54:58 +0800

    改博客页面 等数据部署先

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,35 +1,43 @@
-import React from 'react';
-import {BookOutlined} from '@ant-design/icons';
-import {Layout, Menu, theme} from 'antd';
+import React, {useState, useEffect} from 'react';
+import {Layout, Menu} from 'antd'
+import {useNavigate} from "react-router-dom"
+import {BookOutlined} from '@ant-design/icons'
+
 import styles from './blog.module.css'
-import {blogMenu} from "../../store/NoLoginData";
-import {useNavigate} from "react-router-dom";
+import {blogMenu} from "../../store/NoLoginData"
 
-const {Content, Sider} = Layout;
 
+// 模拟菜单
 const items = blogMenu.map(item => ({
-    key: item[0],
-    label: item[0],
-    icon: <BookOutlined />,
-    children: item.length > 1 ?
-        item.slice(1).map(child => ({
-        key: child,
-        label: child,
-    })):[]
+    key      : item[0],
+    label    : item[0],
+    icon     : <BookOutlined/>,
+    children : item.length > 1 ? item.slice(1).map(child => ({key: child, label: child})) : []
 }))
+// antd布局组件
+const {Content, Sider} = Layout
 
 
-
-
-
+/**
+ * 博客页
+ * */
 const Blog = () => {
-    const {token: {colorBgContainer}} = theme.useToken();
+    const [content, setContent] = useState(<h2>欢迎来到仰晨博客</h2>);
+    const [menu, setMenu] = useState(items);
     const navigate = useNavigate()   // 路由跳转
 
-    const handleMenuClick = (item, key, keyPath, domEvent) => {
-        navigate(`?key=${encodeURIComponent(item.keyPath)}`);
-        console.log('███████item,key,keyPath,domEvent>>>>', item,key,keyPath,domEvent,'<<<<██████')
-    };
+    /** 页面加载菜单 (和读取URL的菜单) */
+    useEffect(() => {
+        // 请求获取最新菜单
+
+    }, [])
+
+    /** 菜单点击事件 */
+    const handleMenuClick = (item) => {
+        navigate(`?item=${encodeURIComponent(item.keyPath)}`);
+        const [sub, parent] = item.keyPath;
+        setContent(`正在请求${parent}/${sub}，请稍后...`)
+    }
 
 
     return (
@@ -38,13 +46,12 @@ const Blog = () => {
             <Sider width={200}
                    theme={'light'}
                    className={styles.scrollbar}
-                   style={{background: colorBgContainer,overflow: 'auto'}}
+                   style={{overflow: 'auto'}}
                    collapsible
             >
                 <Menu
                     mode="inline"
-                    style={{height: '100%', borderRight: 0}}
-                    items={items}
+                    items={menu}
                     onClick={handleMenuClick}
                 />
             </Sider>
@@ -52,13 +59,11 @@ const Blog = () => {
             {/*------ 页面右侧 -------*/}
             <Layout style={{padding: '0 24px 24px'}}>
                 <br/>
-                <Content className={[styles.scrollbar, styles.content].join(' ')}>
-                    Content
-                    <p>怎么动态变化呢</p>
+                <Content className={`${styles.scrollbar} ${styles.content}`}>
+                    {content}
                 </Content>
             </Layout>
-
         </Layout>
-    );
-};
-export default Blog;
+    )
+}
+export default Blog
