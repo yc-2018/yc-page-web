4dcad6b6e8fce007d73c9844f07b1688a0a0bd30
仰晨 <cgl556@88.com>
2024-03-21 17:47:14 +0800

    md组件 优化中

--- src/compontets/Md/index.jsx
+++ src/compontets/Md/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Markdown from 'markdown-to-jsx';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import {a11yDark} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
+import styles from './md.module.css'
 
 // 创建自定义主题，继承原有主题并添加单行代码样式
 const customTheme = {
@@ -15,9 +16,8 @@ const customTheme = {
     // 可能还需要进一步自定义其他部分，具体取决于你希望修改哪些样式
 };
 
-const CodeBlock = ({ children, className }) => {
-    // console.log('███████children>>>>', className,'<<<<██████')
-    const isSingleLine = !/\n/.test(children);      // 判断是否为单行代码 还是有很大问题的
+const CodeBlock = ({children, className}) => {
+    const isSingleLine = !/\n/.test(children) && !className    // 判断是否为单行代码 还是有点问题的
 
     // 使用不同的样式来区分单行和多行代码
     return (
@@ -30,47 +30,48 @@ const CodeBlock = ({ children, className }) => {
     );
 };
 
-export default ({ children }) => {
+export default ({children}) => {
     const options = {
         overrides: {
-            code: { component: CodeBlock },
+            code: {component: CodeBlock},
             blockquote: {   // 引用块
-                component: props => (
-                    <blockquote style={{
-                        borderLeft: '4px solid #ccc',
-                        margin: '1.5em 10px',
-                        padding: '0 0 0 10px',
-                    }}>
+                component: props =>
+                    <blockquote className={styles.blockquote}>
                         {props.children}
                     </blockquote>
-                ),
             },
             table: {
-                component: props => (
-                    <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+                component: props =>
+                    <table style={{borderCollapse: 'collapse', width: '100%'}}>
                         {props.children}
                     </table>
-                ),
             },
             th: {
-                component: props => (
-                    <th style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>
+                component: props =>
+                    <th style={{border: '1px solid #ddd', padding: '8px', textAlign: 'left'}}>
                         {props.children}
                     </th>
-                ),
+            },
+            tr: {
+                component: props => <tr className={styles.tr}>{props.children}</tr>
             },
             td: {
-                component: props => (
-                    <td style={{ border: '1px solid #ddd', padding: '8px' }}>
+                component: props =>
+                    <td style={{border: '1px solid #ddd', padding: '8px'}}>
                         {props.children}
                     </td>
-                ),
             },
+            kbd: {
+                component: props =>
+                    <kbd className={styles.kbd}>
+                        {props.children}
+                    </kbd>
+            },
+            hr: {component: () => <hr className={styles.hr}/>},
         },
 
     };
 
-    return (
-            <Markdown options={options}>{children}</Markdown>
-    );
+    return <Markdown options={options}>{children}</Markdown>
+
 };
