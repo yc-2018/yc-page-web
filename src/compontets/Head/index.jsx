66bc822f37e65fb85bda4e9a5e6ff238feb57214
仰晨 <cgl556@88.com>
2024-03-22 23:59:49 +0800

    头部 优化

--- src/compontets/Head/index.jsx
+++ src/compontets/Head/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import {Avatar, Col, Layout, Menu, Row} from "antd";
 import {Header} from "antd/es/layout/layout";
-import {items} from "../../store/NoLoginData";
-import {useLocation, useNavigate} from "react-router-dom";
 import {UserOutlined} from "@ant-design/icons";
+import {Avatar, Col, Layout, Menu, Row} from "antd";
+import {useLocation, useNavigate} from "react-router-dom";
 
-const Head = () => {
+import {items} from "../../store/NoLoginData";
+
+
+export default () => {
     const navigate = useNavigate()   // 路由跳转
-    const location = useLocation()      // 获取当前路径就靠它
+    const location = useLocation()          // 获取当前路径就靠它
 
     /** 头部页面跳转 */
-    const toPage = (e) => {
-        if (e.domEvent.target.localName === 'a') return;   // 点击a标签跳转 不需要改变路径
+    const toPage = e => {
+        if (e.domEvent.target.localName === 'a') return   // 点击a标签跳转 不需要改变路径
         navigate(e.key)
-    };
-
+    }
 
     return (
         <Layout>
@@ -23,7 +24,6 @@ const Head = () => {
                 alignItems: 'center',
                 background: 'linear-gradient(100deg, #f9e4e4 0%, #afcdcc 100%)',
                 boxShadow: '0 9px 66px 1px #39487061',
-
             }}>
                 <Row style={{width: '100%'}}>
                     <Col span={1}><Avatar src="/favicon.ico" onClick={() => navigate('/')} style={{cursor: 'pointer'}}/></Col>
@@ -41,4 +41,3 @@ const Head = () => {
         </Layout>
     );
 };
-export default Head;
