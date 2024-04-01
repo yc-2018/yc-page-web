2ac2a8c95ac5175c08eee346997de829c885159d
仰晨 <cgl556@88.com>
2024-04-01 11:45:37 +0800

    感觉不能动

--- src/compontets/InfoModal/index.jsx
+++ src/compontets/InfoModal/index.jsx
@@ -1,17 +1,47 @@
 import {observer} from "mobx-react-lite";
-import {Modal} from "antd";
+import {Form, Input, Modal} from "antd";
 import UserStore from "../../store/UserStore";
-import {useState} from "react";
+import React, {useState} from "react";
+import JWTUtils from "../../utils/JWTUtils";
+import {updateNameOrAvatar} from "../../request/commonRequest";
 
 export default observer(() => {
     const [confirmLoading, setConfirmLoading] = useState(false) // 是否加载中
+    const [infoForm] = Form.useForm()
+
+    /**点击确定按钮事件*/
+    const handleOk = async () => {
+        infoForm.validateFields().then(async values => {
+            const {username, avatar} = values;
+            setConfirmLoading(true)
+            const funcResult = await updateNameOrAvatar({username, avatar})
+            setConfirmLoading(false)
+            if (funcResult === 1) {
+                UserStore.setInfoModal(false)
+                infoForm.resetFields()
+            }
+        }).catch((e) => console.log('表单有误=>', e))
+    }
 
     return (
-        <Modal open={UserStore.infoModal}
+        <Modal onOk={handleOk}
+               open={UserStore.infoModal}
                confirmLoading={confirmLoading}
                onCancel={()=>UserStore.setInfoModal(false)}
         >
+            <Form
+                form={infoForm}
+                layout="vertical"
+                initialValues={{name: JWTUtils.getName(), avatar: JWTUtils.getAvatar()}}
+            >
+                <Form.Item name="username" label={'用户昵称'}>
+                    <Input placeholder={'请输入用户昵称'} />
+                </Form.Item>
 
+                <Form.Item name="avatar" label="头像">
+                    <Input placeholder="请输入网址" />
+                </Form.Item>
+            </Form>
         </Modal>
     )
 })
--- src/request/commonRequest.js
+++ src/request/commonRequest.js
@@ -4,35 +4,36 @@ import CommonStore from "../store/CommonStore";
 import UserStore from "../store/UserStore";
 import myAxios from "./myAxios";
 
+const {msg} = CommonStore;
+
 /** 用户登录 */
-export async function login(loginCode,expireTime='bt', loading) {
+export async function login(loginCode, expireTime = 'bt', loading) {
     try {
         loading && loading(true)
         const response = await axios.post(`/api/users/login?key=${loginCode}&expireTime=${expireTime}`);
         loading && loading(false)
-        const {code,msg,data} = response.data;
+        const {code, msg, data} = response.data;
 
         if (code === 1) {
             // 存储 JWT
             UserStore.jwt = data;
-            CommonStore.msg.success("登录成功");
+            msg.success("登录成功");
             return true;
-        } else if (code === 0) CommonStore.msg.error(msg);  // 显示消息
+        } else if (code === 0) msg.error(msg);  // 显示消息
     } catch (error) {
-        CommonStore.msg.error('请求失败');
+        msg.error('请求失败');
     }
 }
 
 export async function updateNameOrAvatar(body) {
     try {
-        const response = await myAxios.put(`/api/users`,body);
-        const {code,msg,data} = response.data;
+        const response = await myAxios.put(`/api/users`, body);
+        const {code, msg, data} = response.data;
         if (code === 1) {
             // 保存新的
             UserStore.jwt = data;
-            CommonStore.msg.success("更新成功");
-            return true;
-        } else CommonStore.msg.error(msg);  // 显示消息
-    } catch (error) {
-    }
+            msg.success("更新成功");
+            return 1;
+        } else msg.error(msg);  // 显示消息
+    } catch (_err) {msg.error('操作失败')}
 }
