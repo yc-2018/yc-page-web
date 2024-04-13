bb4a23a4305d24deb4d51a15ed34b51c766e72f4
仰晨 <cgl556@88.com>
2024-04-14 00:38:24 +0800

    优化博客图标名

--- src/pages/Blog/iconMapping.js
+++ src/pages/Blog/iconMapping.js
@@ -6,22 +6,20 @@ import { ReactComponent as Js } from './svg/js.svg'
 import { ReactComponent as Linux } from './svg/linux.svg'
 import { ReactComponent as Py } from './svg/py.svg'
 import { ReactComponent as SpringBoot } from './svg/springboot.svg'
-import { ReactComponent as Server } from './svg/server.svg'
 import { ReactComponent as Win } from './svg/win.svg'
 import { ReactComponent as OtherIssues } from './svg/otherIssues.svg'
 
 
 export default {
-    'CSS相关': <CSS/>,
-    'docker问题':<Docker/>,
-    'Git问题':<Git/>,
-    'java问题':<Java/>,
-    'js问题':<Js/>,
-    'Linux相关':<Linux/>,
-    'py问题':<Py/>,
-    'Spring boot 问题':<SpringBoot/>,
-    '关于服务器':<Server/>,
-    '玩转win':<Win/>,
+    'CSS': <CSS/>,
+    'Docker':<Docker/>,
+    'Git':<Git/>,
+    'Java':<Java/>,
+    'JavaScript':<Js/>,
+    'Linux':<Linux/>,
+    'Python':<Py/>,
+    'SpringBoot':<SpringBoot/>,
+    'Windows':<Win/>,
     '其他问题':<OtherIssues/>,
 
 
