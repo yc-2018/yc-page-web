92959596ce89699da7ade9972cfeec63cdddb6da
仰晨 <cgl556@88.com>
2024-04-20 16:19:36 +0800

    页面配置单独抽取出来一个浮动按钮   快速搜索图标透明度可以拖动变换

--- src/store/SearchStore.ts
+++ src/store/SearchStore.ts
@@ -3,6 +3,7 @@ import { makeAutoObservable } from 'mobx'
 class SearchStore {
   searchEngines: string = localStorage.getItem('searchEngines') || 'Bing'
   quickSearchIcon: any = localStorage.getItem('quickSearchIcon') ?? true
+  _searchIconTransparency:any = localStorage.getItem('searchIconTransparency') ?? 88
 
   
     //构造函数
@@ -20,6 +21,17 @@ class SearchStore {
       localStorage.setItem('quickSearchIcon', quickSearchIcon ? 'ikun' : '')
       this.quickSearchIcon = quickSearchIcon;
     }
+
+    setSearchIconTransparency(searchIconTransparency:number) {
+      // localStorage.setItem('searchIconTransparency', searchIconTransparency.toString())
+      this._searchIconTransparency = searchIconTransparency;
+    }
+
+    get searchIconTransparency(){
+      // 转数字再输出
+      return parseInt(this._searchIconTransparency)
+    }
+
     // get print() {
     //   //可以把本来的值美化在输出
     //   return null
