8ca0b3d8acf26a096dd07d19a0141c7f695e9977
仰晨 <cgl556@88.com>
2024-07-20 03:44:32 +0800

    // 备忘录搜索关键词高亮不转义前 搜索包涵  ‘[’    就会白屏

--- src/utils/HighlightKeyword.js
+++ src/utils/HighlightKeyword.js
@@ -6,8 +6,9 @@ import React from 'react';
 */
 const HighlightKeyword = ({ content, keyword }) => {
   if (!content || !keyword) return content;
-  
-  const regex = new RegExp(`(${keyword})`, 'gi');
+
+  const escapedKeyword = escapeRegExp(keyword); // 转义关键字，防止正则表达式匹配到特殊字符
+  const regex = new RegExp(`(${escapedKeyword})`, 'gi');
   const parts = content.split(regex);
   let j = 0;  // 为什么不用i，因为i在三元表达式中，有可能出现1、3、5...全是同色
   return (
@@ -28,3 +29,13 @@ const styles = [
   {backgroundColor: '#0400ff29',color:'#000'},
   {backgroundColor: '#ff000029',color:'#000'},
 ]
+
+/**
+ * 使用正则表达式替换所有可能的特殊字符
+ *
+ * @author Yc
+ * @since 2024/7/20 3:40
+ */
+const escapeRegExp = (string) => {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& 表示整个被匹配的字符串
+};
