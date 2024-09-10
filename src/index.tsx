9a25b05af2b26ff19f85de04eb239da53fe65bdd
仰晨 <cgl556@88.com>
2024-09-11 02:25:54 +0800

    小改

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
-import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';    // 导入（日历）中文本地化文件
 import zhCN from 'antd/locale/zh_CN';
-import { ConfigProvider } from 'antd';
+import {ConfigProvider} from 'antd';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 import App from './App';
 import './common.module.css';
+// import dayjs from 'dayjs';
+// import updateLocale from 'dayjs/plugin/updateLocale';
+
 // import reportWebVitals from './reportWebVitals';     // 引入性能测试
 
+// 修改周的起始日？
+// dayjs.extend(updateLocale);
+// dayjs.updateLocale('zh-cn', {
+//   weekStart: 0,
+// });
 
-dayjs.locale('zh-cn');   // 设置本地化为中文(日历组件)
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <ConfigProvider locale={zhCN} theme={{components: {Message: {zIndexPopup: 9999999}}}}>
-        <BrowserRouter>
-            <React.StrictMode>
-                <App/>
-            </React.StrictMode>
-        </BrowserRouter>
-    </ConfigProvider>
+  <ConfigProvider locale={zhCN} theme={{components: {Message: {zIndexPopup: 9999999}}}}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App/>
+      </React.StrictMode>
+    </BrowserRouter>
+  </ConfigProvider>
 )
-
 
 
 // 如果您想开始在应用程序中测量性能，请传递一个函数
