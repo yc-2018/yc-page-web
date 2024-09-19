afb556e14597aed74e4eb7571232e1813489bab9
仰晨 <cgl556@88.com>
2024-09-20 01:23:26 +0800

    看时间 周 月 年 正常显示

--- src/pages/SeeTime/interface.ts
+++ src/pages/SeeTime/interface.ts
@@ -1,10 +1,9 @@
 export interface SeeData {
-  id: number;
   startTime: string;
-  endTime: string;
-  remark: string;
+  endTime?: string;
+  remark?: string;
   thisTime: number;
   totalDuration: number;
-  date:string;            // 聚合后日期
-  count: number;          // 聚合后计次
+  date?:string;            // 聚合后日期
+  count?: number;          // 聚合后计次
 }
