0d735b9c5f68e0b0d2de6460e49837657aec92b6
仰晨 <cgl556@88.com>
2025-03-17 20:03:46 +0800

    feat(Mobile): 优化备忘录编辑界面功能和样式 https://img20.360buyimg.com/openfeedback/jfs/t1/229521/36/37585/93047/67d80f4bFe79c84f1/5c6c24fda2a29b43.png - 调整了类型选择按钮的位置，使其更加显眼 - 增加了插入符号功能，提供多种符号供用户选择- 优化了文本域样式，增加了边框和圆角 - 调整了Radio按钮的样式，使其更加紧凑 -移除了不必要的红星前缀样式

--- src/pages/Mobile/Memo.jsx
+++ src/pages/Mobile/Memo.jsx
@@ -3,7 +3,7 @@ import {
   InfiniteScroll, List, Popup, SwipeAction, Toast,
   Button, Tag, Radio, TextArea, Dialog, PullToRefresh,
   SearchBar, Badge, Ellipsis, CalendarPicker, Dropdown,
-  Space, Input
+  Space, Input, Modal
 } from 'antd-mobile'
 
 import {delToDoItem, getToDoItems, saveOrUpdateToDoItem, selectLoopMemoTimeList} from "../../request/memoRequest.js";
@@ -13,6 +13,7 @@ import {sortingOptions} from "../../store/NoLoginData.jsx";
 import styles from './mobile.module.css'
 import HighlightKeyword from "../../utils/HighlightKeyword.jsx";
 import dayjs from "dayjs";
+import {symbols} from "../MemoDrawer/compontets/FormModal.jsx";
 
 
 let updateTime;     // 待办更新时间
@@ -506,35 +507,59 @@ const Memo = ({type, setIncompleteCounts, changeType, setChangeType}) => {
       >
 
         <div style={{padding: '10px'}}>
-          <div className={'█required editBoxTitle'}>
-            内容 &nbsp;
-            <Button size="small" onClick={() => setEditDateVisible(true)}>插入日期</Button>
+          
+          <div style={{textAlign: 'center'}}>
+            <Radio.Group value={itemType} onChange={value => setItemType(() => value)}>
+              <Radio value={0} className={'█Radio'}>普通</Radio>
+              <Radio value={1} className={'█Radio'}>循环</Radio>
+              <Radio value={2} className={'█Radio'}>长期</Radio>
+              <Radio value={3} className={'█Radio'}>紧急</Radio>
+              <Radio value={5} className={'█Radio'}>日记</Radio>
+              <Radio value={6} className={'█Radio'}>工作</Radio>
+              <Radio value={7} className={'█Radio'}>其他</Radio>
+            </Radio.Group>
           </div>
+          
           <TextArea
             rows={13}
             showCount
             ref={textRef}
             value={content}
             className="contentText"
-            style={{height: '250px'}}
+            style={{height: '300px'}}
             placeholder="请输入备忘内容"
             maxLength={itemType === 5 ? 4000 : 2000}
             onChange={value => setContent(value)}
           />
-          <br/>
-          <div className={'█required'}>请选择类型</div>
-          <Radio.Group value={itemType} onChange={value => setItemType(() => value)}>
-            <Radio value={0} className={'█Radio'}>普通</Radio>
-            <Radio value={1} className={'█Radio'}>循环</Radio>
-            <Radio value={2} className={'█Radio'}>长期</Radio>
-            <Radio value={3} className={'█Radio'}>紧急</Radio>
-            <Radio value={5} className={'█Radio'}>日记</Radio>
-            <Radio value={6} className={'█Radio'}>工作</Radio>
-            <Radio value={7} className={'█Radio'}>其他</Radio>
-          </Radio.Group>
-          <br/>
-          <br/>
-          <br/>
+          <div style={{margin: '10px 0'}}>
+            <Button size="small" onClick={() => setEditDateVisible(true)}>插入日期</Button>
+            &nbsp;
+            <Button
+              size="small"
+              onClick={() =>{
+                const handler =  Modal.show({
+                  content:
+                    <div style={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: 2}}>
+                      {symbols.map((item) =>
+                        <div
+                          key={item}
+                          style={{padding: 5, border: '1px solid #ccc', textAlign: 'center'}}
+                          onClick={() => insertAtCursor(item)||handler.close()}
+                        >
+                          {item}
+                        </div>
+                      )}
+                    </div>,
+                  closeOnMaskClick: true,
+                  closeOnAction: true,
+                  actions: [{key: 'close', text: '关闭'}]
+                })
+              }}
+            >
+              插入符号
+            </Button>
+          </div>
+          
           <Button block onClick={submit}> 提交 </Button>
         </div>
       </Popup>
@@ -561,7 +586,7 @@ const Memo = ({type, setIncompleteCounts, changeType, setChangeType}) => {
       </Popup>
 
 
-      {/*日期选择器（antd实验性组件）*/}
+      {/*日期选择器（antd实验性组件）完成或+1 用 */}
       <CalendarPicker
         popupStyle={{zIndex: 99999}}
         min={new Date(Date.now() - 6 * 24 * 60 * 60 * 1000)}    // 前7天
@@ -580,7 +605,7 @@ const Memo = ({type, setIncompleteCounts, changeType, setChangeType}) => {
         }}
       />
       
-      {/*日期选择器（antd实验性组件）*/}
+      {/*日期选择器（antd实验性组件）编辑插入用*/}
       <CalendarPicker
         popupStyle={{zIndex: 99999}}
         visible={editDateVisible}
