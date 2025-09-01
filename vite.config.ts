9bca097f4ab4c7b417ec3507753c4fbe0797295d
仰晨 <cgl556@88.com>
2025-09-01 11:17:30 +0800

    refactor(vite.config.ts): 更新 path 模块导入方式
    - 将 path 模块的默认导入改为命名空间导入
    -保持现有功能不变，仅修改导入方式

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'path'
 // import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
